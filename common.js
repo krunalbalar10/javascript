abce1fcc51d3c9b1d8aff78f143c6a1e8f378634
krunalbalar10 <bholu2104patel@gmail.com>
2023-12-09 13:55:39 +0530

    loop

--- common.js
+++ common.js
@@ -2,3 +2,10 @@ function $(TagId){
     let id = document.getElementById(TagId);
     return id;
 }
+
+let log = (msg1 , msg2='not given' , msg3='not given') => {
+    console.log(msg1 , msg2 , msg3);
+}
+
+let int = (num) => parseInt(num);
+let float = (num) => parseFloat(num);

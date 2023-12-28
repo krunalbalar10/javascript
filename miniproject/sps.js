71eee0a8c136f9a48b352e3d700764687fc52b8f
krunalbalar10 <bholu2104patel@gmail.com>
2023-12-28 21:17:32 +0530

    rock paper scissor game

--- miniproject/sps.js
+++ miniproject/sps.js
@@ -3,9 +3,82 @@ let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
 
+const msg = document.querySelector("#msg");
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+//third step
+const genCompChoice = () => {
+    const option = ["rock" , "paper" , "scissors"];
+    // math.floor is used for remove desimal number after point
+    // like 2.34334345 output gives  2.
+    // math.random used for taking any number if you want specific range then you
+    //have to use * 3 will give you 0 to 2 number .here you can multiply any number 
+    const randIdx = Math.floor(Math.random() * 3);
+    return option[randIdx];
+    //rock , paper , scissors
+}
+
+//forth step
+const drawGame = () => {
+    console.log("Game is draw.");
+    msg.innerText = "Game is Draw. Play again";
+    msg.style.backgroundColor = "blue";
+
+}
+
+//fifth step
+const showWinner = (userWin , userChoice , compChoice) => {
+    if (userWin){
+        userScore++;
+        userScorePara.innerText = userScore;
+        msg.innerText = `You win! Your ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor = "green";
+    }
+    else
+    {
+        compScore++;
+        compScorePara.innerText = compScore;
+        msg.innerText = `You Lose! ${compChoice} beats your ${compChoice}`;
+        msg.style.backgroundColor = "red";
+
+    }
+};
+
+//second step
+const playGame = (userChoice) => {
+    console.log("user choice = ", userChoice);
+    //Generate computer choice
+    const compChoice = genCompChoice();
+    console.log("Computer choice = ", compChoice);
+
+    if(userChoice === compChoice){
+        drawGame();
+    }
+    else{
+        let userWin = true;
+        if(userChoice === "rock"){
+            //scissors , paper
+            userWin = compChoice === "paper"? false: true;
+        }
+        else if(userChoice === "paper")
+        {
+            //rock , scissor
+            userWin = compChoice === "scissors" ? false: true;
+        }
+        else {
+            //rock , paper
+            userWin = compChoice === "rock" ? false:true;
+        }
+        showWinner(userWin , userChoice , compChoice);
+    }
+}
+
+//first step
 choices.forEach((choice) => {
     console.log(choice);
     choice.addEventListener("click" , () => {
-        console.log("choice was clicked");
-    });
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+;    });
 });
